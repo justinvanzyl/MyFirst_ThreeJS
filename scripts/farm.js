64f76b61490d95b6eff038bfc6329c35148240d8
justinvanzyl <justin.v.zyl@gmail.com>
2020-09-08 04:51:40 +0200

    v1

--- scripts/farm.js
+++ scripts/farm.js
@@ -35,18 +35,20 @@ function init() {
 	sunlight.position.set(150, 200, 50);
 	scene.add(sunlight);
 
+	var geometry;
+	var material;
 	// Building the farm:
 	farm = new THREE.Group();
 	// Creating the ground:
-	var geometry = new THREE.PlaneGeometry(100, 100);
-	var material = new THREE.MeshStandardMaterial({ color: 0x567D46 });
+	geometry = new THREE.PlaneGeometry(100, 100);
+	material = new THREE.MeshStandardMaterial({ color: 0x567D46 });
 	var ground = new THREE.Mesh(geometry, material);
 	ground.rotateX(-1.5708);
 	farm.add(ground);
 
 	// Creating the watertank:
-	var geometry = new THREE.CylinderGeometry(6, 6, 8, 32);
-	var material = new THREE.MeshStandardMaterial({ color: 0xADB2BD });
+	geometry = new THREE.CylinderGeometry(6, 6, 8, 32);
+	material = new THREE.MeshStandardMaterial({ color: 0xADB2BD });
 	var watertank = new THREE.Mesh(geometry, material);
 	watertank.position.set(-30, 8, 35);
 	farm.add(watertank);
@@ -55,9 +57,9 @@ function init() {
 	house = new THREE.Group();
 
 	//first the walls:
-	var geometry = new THREE.BoxGeometry(25, 10, 20);
-	var materials = new THREE.MeshStandardMaterial({ color: 0xEDE9CE });
-	var walls = new THREE.Mesh(geometry, materials);
+	geometry = new THREE.BoxGeometry(25, 10, 20);
+	material = new THREE.MeshStandardMaterial({ color: 0xEDE9CE });
+	var walls = new THREE.Mesh(geometry, material);
 	walls.position.set(0, 5, 0);
 	house.add(walls);
 
@@ -73,8 +75,8 @@ function init() {
 		depth: 22,
 		bevelEnabled: false,
 	};
-	var geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
-	var material = new THREE.MeshStandardMaterial({ color: 0x996633 });
+	geometry = new THREE.ExtrudeGeometry(shape, extrudeSettings);
+	material = new THREE.MeshStandardMaterial({ color: 0x996633 });
 	var roof = new THREE.Mesh(geometry, material);
 	roof.rotateZ(-2.355);
 	roof.scale.set
@@ -87,34 +89,48 @@ function init() {
 
 
 	// Creating trees:
-	var tree = new THREE.Group()
+	var woods = [];
+	var tree = new THREE.Group();
 
 	// the trunk:
-	var geometry = new THREE.CylinderGeometry(1, 1, 10);
-	var material = new THREE.MeshStandardMaterial({ color: 0x553318 });
+	geometry = new THREE.CylinderGeometry(1, 1, 10);
+	material = new THREE.MeshStandardMaterial({ color: 0x553318 });
 	var trunk = new THREE.Mesh(geometry, material);
 	trunk.position.y = 5;
 	tree.add(trunk);
 
 	// the crown:
-	var geometry = new THREE.ConeGeometry(3, 12, 32);
-	var material = new THREE.MeshStandardMaterial({ color: 0x476A30 });
+	geometry = new THREE.ConeGeometry(3, 12, 32);
+	material = new THREE.MeshStandardMaterial({color: 0x476A30});
 	var crown = new THREE.Mesh(geometry, material);
 	crown.position.y = 10;
 	tree.add(crown);
-	tree.position.set(-20, 0, -10);
-	farm.add(tree);
+
+	for (var i = 0; i < 10; i++) {
+		woods.push(tree.clone());
+		farm.add(woods[i]);
+	}
+	woods[0].position.set(-45,0,-45);
+	woods[1].position.set(-30, 0, -36);
+	woods[2].position.set(-15, 0, -37);
+	woods[3].position.set(-38,0,-32);
+	woods[4].position.set(-25, 0, -30);
+	woods[5].position.set(-33, 0, -25);
+	woods[6].position.set(-35,0,-43);
+	woods[7].position.set(-40, 0, -15);
+	woods[8].position.set(-28, 0, 20);
+	woods[9].position.set(35,0,-30);
 
 
 	// the igloo:
-	var geometry = new THREE.SphereGeometry(7, 32, 32);
-	var material = new THREE.MeshStandardMaterial({ color: 0xA5F2F3, shading: THREE.SmoothShading });
+	geometry = new THREE.SphereGeometry(7, 32, 32);
+	material = new THREE.MeshStandardMaterial({ color: 0xA5F2F3});
 	var shape1 = new THREE.Mesh(geometry, material);
 	shape1.position.y = 5;
 	var ball = new ThreeBSP(shape1);
 
-	var geometry = new THREE.BoxGeometry(14, 10, 14);
-	var material = new THREE.MeshStandardMaterial({ color: 0xA5F2F3 });
+	geometry = new THREE.BoxGeometry(14, 10, 14);
+	material = new THREE.MeshStandardMaterial({ color: 0xA5F2F3 });
 	var shape2 = new THREE.Mesh(geometry, material);
 	shape2.position.y = 0;
 	var subtraction = new ThreeBSP(shape2);
@@ -175,6 +191,20 @@ function updateSpeed(value) {
 	settings.speed = value/100; //range is from 0.01 to 0.1
 }
 
+function increaseZoom() {
+	settings.zoom += .5;
+	document.getElementById("zoom").value++;
+	if (settings.zoom > 5)
+		settings.zoom = 5;
+}
+
+function decreaseZoom() {
+	settings.zoom -= .5;
+	document.getElementById("zoom").value--;
+	if (settings.zoom < .5)
+		settings.zoom = .5;
+}
+
 // Context Menu event handler:
 window.addEventListener("contextmenu", function(event) {
 	event.preventDefault();
@@ -184,7 +214,7 @@ window.addEventListener("contextmenu", function(event) {
 	contextElement.classList.add("active");
 });
 
-// Click-event handler:
+// Click-event handler (for context menu):
 window.addEventListener("click", function() {
 	document.getElementById("context-menu").classList.remove("active");
 });
